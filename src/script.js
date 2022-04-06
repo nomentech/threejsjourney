d5812424970e61aea2d6f8a8f8f3dbf68778d16f
Nomad <nomad@nomads-mbp.lan>
2022-04-06 15:33:27 -0400

    geometry

--- src/script.js
+++ src/script.js
@@ -40,10 +40,22 @@ const canvas = document.querySelector(".webgl");
 
 const scene = new THREE.Scene();
 
-const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x0000ff})
-);
+const geometry = new THREE.BufferGeometry()
+const count = 500;
+const positionArray = new Float32Array(count*3*3);
+for (let i=0; i< count*3*3; i++) {
+  positionArray[i] = (Math.random() - 0.5) * 4;
+}
+const positionAttribute = new THREE.BufferAttribute(positionArray, 3);
+geometry.setAttribute("position", positionAttribute);
+
+const material = new THREE.MeshBasicMaterial({ 
+  color: 0x0000ff,
+  wireframe: true
+})
+
+const mesh = new THREE.Mesh(geometry, material);
+
 scene.add(mesh);
 
 const camera = new THREE.PerspectiveCamera(75, size.width/size.height, 0.1, 100);
