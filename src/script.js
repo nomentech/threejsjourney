9b7a4b3dd5bfac14313e45a4a4ce229a5b1905f8
Nomad <nomad@nomads-mbp.lan>
2022-04-06 13:51:25 -0400

    fullscreen and resize

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
@@ -7,14 +8,36 @@ const cursor = {
 }
 
 addEventListener("mousemove", (event) => {
-  cursor.x = event.clientX / width - 0.5;
-  cursor.y = event.clientY / height - 0.5;
+  cursor.x = event.clientX / size.width - 0.5;
+  cursor.y = event.clientY / size.height - 0.5;
 });
+
+const size = {
+  width: innerWidth,
+  height: innerHeight
+}
+
+addEventListener("resize", () => {
+  size.width = innerWidth;
+  size.height = innerHeight;
+
+  camera.aspect = size.width / size.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(size.width, size.height);
+  renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
+})
+
+addEventListener("dblclick", () => {
+  if(!document.fullscreenElement) {
+    canvas.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+})
 
 const canvas = document.querySelector(".webgl");
 
-const width = 400;
-const height = 300;
 const scene = new THREE.Scene();
 
 const mesh = new THREE.Mesh(
@@ -23,9 +46,7 @@ const mesh = new THREE.Mesh(
 );
 scene.add(mesh);
 
-// const aspectRatio = width / height;
-// const camera = new THREE.OrthographicCamera(-1*aspectRatio,1*aspectRatio,1,-1,0.1,100);
-const camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(75, size.width/size.height, 0.1, 100);
 camera.position.z = 3;
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
@@ -33,19 +54,10 @@ controls.enableDamping = true;
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 });
-renderer.setSize(width, height);
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
 
-// const clock = new THREE.Clock();
 const tick = () => {
-  // const elapsedTime = clock.getElapsedTime();
-  // mesh.rotation.y = elapsedTime;
-
-  // custom controls
-  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
-  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
-  // camera.position.y = -cursor.y * 5;
-  // camera.lookAt(mesh.position);
-
   controls.update();
   
   renderer.render(scene, camera);
