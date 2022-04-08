081c44b92e586420893a469206a063ad83bd09aa
Nomad <nomad@nomads-mbp.lan>
2022-04-08 13:40:15 -0400

    light

--- src/script.js
+++ src/script.js
@@ -1,9 +1,8 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
-import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
-import { TextureLoader } from "three";
+import { RectAreaLightHelper } from "three/examples/jsm/helpers/RectAreaLightHelper";
+
 const size = {
   width: innerWidth,
   height: innerHeight
@@ -30,57 +29,81 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(size.width, size.height);
 renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
 
-// Texture
-const textureLoader = new TextureLoader();
-const matcapTexture = textureLoader.load("textures/matcaps/1.png");
+// Material
+const material = new THREE.MeshStandardMaterial();
+material.roughness = 0.4;
 
-// Fonts
-const fontLoader = new FontLoader();
-fontLoader.load(
-  "fonts/helvetiker_regular.typeface.json", 
-  (font) => {
-    const textGeometry = new TextGeometry(
-    "Hello Three.js", 
-    {
-      font: font,
-      size: 0.5,
-      height: 0.2,
-      curveSegments: 5,
-      bevelEnabled: true,
-      bevelThickness: 0.03,
-      bevelSize: 0.02,
-      bevelOffset: 0,
-      bevelSegments: 3
-    });
-    // textGeometry.computeBoundingBox();
-    // textGeometry.translate(
-    //   -(textGeometry.boundingBox.max.x - 0.02)*0.5,
-    //   -(textGeometry.boundingBox.max.y - 0.02)*0.5,
-    //   -(textGeometry.boundingBox.max.z - 0.03)*0.5,
-    // )
-    textGeometry.center();
-    const material = new THREE.MeshMatcapMaterial();
-    material.matcap = matcapTexture;
-    const text = new THREE.Mesh(textGeometry, material);
-    scene.add(text);
-
-    const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 10, 20);
-    for(let i=0; i<100; i++) {
-      const donutMesh = new THREE.Mesh(donutGeometry, material);
-      donutMesh.position.x = (Math.random()-0.5)*10;
-      donutMesh.position.y = (Math.random()-0.5)*10;
-      donutMesh.position.z = (Math.random()-0.5)*10;
-      
-      donutMesh.rotation.x = Math.random() * Math.PI;
-      donutMesh.rotation.y = Math.random() * Math.PI;
-
-      const scale = Math.random();
-      donutMesh.scale.set(scale, scale, scale);
-
-      scene.add(donutMesh);
-    }
-  }
+// Mesh
+const sphere = new THREE.Mesh(
+  new THREE.SphereBufferGeometry(0.5, 32, 32),
+  material
 );
+sphere.position.x = -1.5;
+
+const cube = new THREE.Mesh(
+  new THREE.BoxBufferGeometry(0.75, 0.75, 0.75),
+  material
+);
+
+const torus = new THREE.Mesh(
+  new THREE.TorusBufferGeometry(0.3, 0.2, 32, 64),
+  material
+)
+torus.position.x = 1.5;
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(5, 5),
+  material
+);
+plane.rotation.x = - Math.PI * 0.5;
+plane.position.y = -0.65;
+scene.add(sphere, cube, torus, plane);
+
+// Light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
+directionalLight.position.set(1, 0.25, 0);
+scene.add(directionalLight);
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3);
+scene.add(hemisphereLight);
+
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2);
+pointLight.position.set(1, -0.5, 1);
+scene.add(pointLight);
+
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1);
+rectAreaLight.position.set(-1.5, 0, 1.5);
+rectAreaLight.lookAt(new THREE.Vector3());
+scene.add(rectAreaLight);
+
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI*0.1, 0.25, 1);
+spotLight.position.set(0, 2, 3);
+scene.add(spotLight);
+
+spotLight.target.position.x = -0.75;
+scene.add(spotLight.target);
+
+// Light Helper
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2);
+scene.add(hemisphereLightHelper);
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2);
+scene.add(directionalLightHelper);
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2);
+scene.add(pointLightHelper);
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(spotLightHelper);
+requestAnimationFrame(() => {
+  spotLightHelper.update();
+})
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight);
+scene.add(rectAreaLightHelper);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, size.width/size.height, 0.1, 100);
@@ -93,6 +116,13 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  sphere.rotation.x = elapsedTime * 0.1;
+  sphere.rotation.y = elapsedTime * 0.1;
+  cube.rotation.x = elapsedTime * 0.1;
+  cube.rotation.y = elapsedTime * 0.1;
+  torus.rotation.x = elapsedTime * 0.1;
+  torus.rotation.y = elapsedTime * 0.1;
 
   controls.update();
   
