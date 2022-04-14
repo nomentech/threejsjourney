8374c3cfffdb3da8823e896859a90965f81d1de2
Nomad <nomad@nomads-mbp.lan>
2022-04-14 16:41:14 -0400

    shader

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
 import "./style.css";
 import * as THREE from "three";
-import * as dat from "dat.gui";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import * as dat from "dat.gui";
+
+import vertex from "./shaders/vertex.glsl";
+import fragment from "./shaders/fragment.glsl";
+
+const gui = new dat.GUI({ closed: true });
 
 const size = {
   width: innerWidth,
@@ -20,12 +24,6 @@ addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
 })
 
-// dat.gui
-const gui = new dat.GUI({ closed: true });
-const parameters = {
-  envMapIntensity: 5
-}
-
 // Canvas and Scene
 const canvas = document.querySelector(".webgl");
 const scene = new THREE.Scene();
@@ -33,99 +31,50 @@ const scene = new THREE.Scene();
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
-  antialias: true
 });
 renderer.setSize(size.width, size.height);
 renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
-renderer.physicallyCorrectLights = true;
-renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.toneMapping = THREE.ReinhardToneMapping;
-renderer.toneMappingExposure = 3;
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-gui.add(renderer, "toneMapping", {
-  No: THREE.NoToneMapping,
-  Linear: THREE.LinearToneMapping,
-  Reinhard: THREE.ReinhardToneMapping,
-  Cineon: THREE.CineonToneMapping,
-  ACESFilmic: THREE.ACESFilmicToneMapping
-}).onFinishChange(() => {
-  renderer.toneMapping = Number(renderer.toneMapping);
-  updateAllMaterials();
-})
-gui.add(renderer, "toneMappingExposure").min(0).max(10).step(0.001);
-
-// Update All Materials
-const updateAllMaterials = () => {
-  scene.traverse(child => {
-    if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
-      // child.material.envMap = envMap;
-      child.material.envMapIntensity = parameters.envMapIntensity;
-      child.material.needsUpdate = true;
-      child.castShadow = true;
-      child.receiveShadow = true;
-    }
-  })
-}
 
 // Texture
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-const envMap = cubeTextureLoader.load([
-  "textures/environmentMaps/0/px.jpg",
-  "textures/environmentMaps/0/nx.jpg",
-  "textures/environmentMaps/0/py.jpg",
-  "textures/environmentMaps/0/ny.jpg",
-  "textures/environmentMaps/0/pz.jpg",
-  "textures/environmentMaps/0/nz.jpg",
-])
-envMap.encoding = THREE.sRGBEncoding;
-scene.background = envMap;
-scene.environment = envMap;
-gui.add(parameters, "envMapIntensity").min(0).max(10).step(0.001).onChange(updateAllMaterials);
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load("textures/flag-tw.jpg");
 
-// Flight Helmet
-const gltfLoader = new GLTFLoader();
-gltfLoader.load(
-  "models/FlightHelmet/glTF/FlightHelmet.gltf",
-  // "models/hamburger.glb",
-  (gltf) => {
-    gltf.scene.scale.set(10, 10, 10, 10);
-    // gltf.scene.scale.set(0.3, 0.3, 0.3);
-    gltf.scene.position.set(0, -4, 0);
-    gltf.scene.rotation.y = Math.PI * 0.5;
-    scene.add(gltf.scene);
+// Geometry
+const geometry = new THREE.PlaneBufferGeometry(1, 1, 32, 32);
+const count = geometry.attributes.position.count;
+const randoms = new Float32Array(count);
+for(let i=0; i<count; i++) {
+  randoms[i] = Math.random();
+}
+geometry.setAttribute("aRandom", new THREE.BufferAttribute(randoms, 1));
 
-    gui.add(gltf.scene.rotation, "y").min(-Math.PI).max(Math.PI).step(0.001).name("rotation");
-
-    updateAllMaterials();
+// Material 
+const material = new THREE.RawShaderMaterial({
+  vertexShader: vertex,
+  fragmentShader: fragment,
+  uniforms: {
+    uFrequency: { value: new THREE.Vector2(10, 5) },
+    uTime: { value: 0 },
+    uColor: { value: new THREE.Color("orange") },
+    uTexture: { value: texture}
   }
+})
+gui.add(material.uniforms.uFrequency.value, "x").min(0).max(20).step(0.01).name("frequencyX");
+gui.add(material.uniforms.uFrequency.value, "y").min(0).max(20).step(0.01).name("frequencyY");
+
+// Mesh
+const mesh = new THREE.Mesh(
+  geometry,
+  material
 )
+mesh.scale.y = 2/3;
+scene.add(mesh);
 
 // Light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.set(1024, 1024);
-directionalLight.shadow.camera.far = 15;
-// directionalLight.shadow.camera.left = -7;
-// directionalLight.shadow.camera.top = 7;
-// directionalLight.shadow.camera.right = 7;
-// directionalLight.shadow.camera.bottom = -7;
-// directionalLight.shadow.normalBias = 0.05;
-directionalLight.position.set(0.25, 3, -2.25);
-scene.add(directionalLight);
-
-const cameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
-cameraHelper.visible = false;
-scene.add(cameraHelper);
-
-gui.add(directionalLight, "intensity").min(0).max(10).step(0.001).name("lightIntensity");
-gui.add(directionalLight.position, "x").min(-5).max(5).step(0.001).name("lightX");
-gui.add(directionalLight.position, "y").min(-5).max(5).step(0.001).name("lightY");
-gui.add(directionalLight.position, "z").min(-5).max(5).step(0.001).name("lightZ");
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, size.width/size.height, 0.1, 100);
-camera.position.set(4, 1, -4);
+camera.position.set(0.5, -1, 2);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
@@ -138,10 +87,10 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
   const deltaTime = elapsedTime - previousTime;
   previousTime = elapsedTime;
-  
-  // Controls
-  controls.update();
 
+  material.uniforms.uTime.value = elapsedTime;
+
+  controls.update();
   renderer.render(scene, camera);
 
   requestAnimationFrame(tick);
